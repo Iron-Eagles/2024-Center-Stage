c19be621597e4e8004807b31deceda929a9e3e71
Chotorem <114266944+Chotorem@users.noreply.github.com>
2023-09-26 15:58:00 -0500

    Merging hubs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefaultMecanum.java
@@ -0,0 +1,76 @@
+//for copying purposes only. Use this as a starter for any other code you may be working on.
+/*
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import java.util.List;
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+
+public class DefaultMecanum extends LinearOpMode {
+    
+    DcMotor leftFront, rightFront, rightBack, leftBack;
+    ElapsedTime runtime = new ElapsedTime();
+    
+      
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        
+        
+        leftFront = hardwareMap.get(DcMotor.class, "LeftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "RightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "RightBack");
+        leftBack = hardwareMap.get(DcMotor.class, "LeftBack");
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        
+        
+        waitForStart();
+        runtime.reset();
+        
+        while (opModeIsActive()) {
+            
+            double lX1, lY1, rX1, rY1;
+            
+            lX1 = gamepad1.left_stick_x;
+            lY1 = gamepad1.left_stick_y;
+            rX1 = gamepad1.right_stick_x;
+            rY1 = gamepad1.right_stick_y;
+            
+            double a = lY1 + lX1;
+            double b = rY1 - lX1;
+            double c = lY1 - lX1;
+            double d = rY1 + lX1;
+            
+            a = Range.clip(a, -1, 1);
+            b = Range.clip(b, -1, 1);
+            c = Range.clip(c, -1, 1);
+            d = Range.clip(d, -1, 1);
+            
+            leftFront.setPower(a);
+            rightFront.setPower(b);
+            leftBack.setPower(c);
+            rightBack.setPower(d);
+            
+            
+            
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
+*/
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -1,48 +1,52 @@
-//for copying purposes only
+/*
 
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Tank", group="Linear Opmode")
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
 
-public class DefaultTanktread extends LinearOpMode {
+public class MotorTest extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
-    DcMotor left, right;
+    private DcMotor testMotor;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        left  = hardwareMap.get(DcMotor.class, "Left");
-        right = hardwareMap.get(DcMotor.class, "Right");
+        
+        testMotor = hardwareMap.get(DcMotorEx.class, "TestMotor");
 
-        left.setDirection(DcMotor.Direction.FORWARD);
-        right.setDirection(DcMotor.Direction.FORWARD);
-
+        
+        
+        
         waitForStart();
         runtime.reset();
 
         
         while (opModeIsActive()) {
 
-            double lY1, rY1;
             
-            lY1 = gamepad1.left_stick_y;
-            rY1 = gamepad1.right_stick_y;
-            
-            left.setPower(lY1);
-            right.setPower(rY1);
+            double rotation = gamepad1.left_stick_y;
+            testMotor.setPower(rotation);
 
+            
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
         }
     }
 }
+
+
+*/
+
+
