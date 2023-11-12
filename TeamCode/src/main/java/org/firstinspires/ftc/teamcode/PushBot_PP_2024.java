b8e3eaec1a02b3cc04e3fdd8fd89f64820634955
Torican <114266944+cody-summerson@users.noreply.github.com>
2023-11-12 08:34:47 -0600

    Update PushBot_PP_2024.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushBot_PP_2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushBot_PP_2024.java
@@ -1,4 +1,4 @@
-//for copying purposes only. Use this as a starter for any other code you may be working on.
+//Power Play 2024 Push Bot
 
 package org.firstinspires.ftc.teamcode;
 
@@ -10,22 +10,20 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="_Push Bot - PP 2024", group="Linear Opmode")
 
-public class DefaultMecanum extends LinearOpMode {
+public class PushBot_PP_2024 extends LinearOpMode {
     
     DcMotor leftFront, rightFront, rightBack, leftBack;
     ElapsedTime runtime = new ElapsedTime();
 
     double adjSpeedConstant = 0.4;
     double realSpeed = 1;
-
     boolean turboMode = false, reset = false;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        
-        
+
         leftFront = hardwareMap.get(DcMotor.class, "LeftFront");
         rightFront = hardwareMap.get(DcMotor.class, "RightFront");
         rightBack = hardwareMap.get(DcMotor.class, "RightBack");
@@ -35,25 +33,23 @@ public class DefaultMecanum extends LinearOpMode {
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setDirection(DcMotor.Direction.REVERSE); //tell ELI To fix this motor gear
-        
-        
+
         waitForStart();
         runtime.reset();
         
         while (opModeIsActive()) {
             
-            double lX1, lY1, rX1, rY1;
+            double lX1, lY1, rY1;
             
             lX1 = gamepad1.left_stick_x;
             lY1 = gamepad1.left_stick_y;
-            rX1 = gamepad1.right_stick_x;
             rY1 = gamepad1.right_stick_y;
             
             double a = lY1 - lX1;
             double b = rY1 - lX1;
             double c = lY1 + lX1;
             double d = rY1 + lX1;
-            
+
             a = Range.clip(a, -1, 1);
             b = Range.clip(b, -1, 1);
             c = Range.clip(c, -1, 1);
